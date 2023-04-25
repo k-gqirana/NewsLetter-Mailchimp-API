a6bffd2ab6e03ed9dc152656fde7c9e9ce85837b
k-gqirana <s216057361@mandela.ac.za>
2023-04-25 11:02:24 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req, res) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.secondName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const url = process.env.MAIL_CHIMP_LIST_URL;
+  const options = {
+    method: "POST",
+    auth: process.env.MAIL_CHIMP_AUTH,
+  };
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+app.listen(process.env.PORT || 3000, function () {
+  //Listens for a dynamic port on the heroku servers used to deploy app OR local port when we want to test
+  console.log("Server is running on port 3000");
+});
